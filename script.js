ee6b27299c9573e836ccbb23974cf8a46e493b57
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-07 21:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function askConfirmation() {
+    var confirmation = confirm("Do you want to proceed?");
+
+    if (confirmation) {
+        alert("You clicked OK. Proceeding...");
+    } else {
+        alert("You clicked Cancel. Exiting...");
+    }
+}
